cc2ad312387d56fea06eabd8e45b3e442b77a515
Aditya Alif Nugraha <aditya.nugraha@go-jek.com>
2021-08-14 22:32:38 +0700

    Add json dumps indent param

--- freqtrade/rpc/rabbitmq.py
+++ freqtrade/rpc/rabbitmq.py
@@ -27,6 +27,6 @@ class RabbitMQ(RPCHandler):
         channel = connection.channel()
 
         channel.queue_declare('freqtrade')
-        channel.basic_publish(exchange='', routing_key='freqtrade', body=json.dumps(msg))
+        channel.basic_publish(exchange='', routing_key='freqtrade', body=json.dumps(msg, indent=4).encode('utf-8'))
 
         connection.close()
